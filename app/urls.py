02958b4bacc1da56e964a73b1e29cc95f8d8af32
unknown <chohankoti333@gmail.com>
2024-02-01 00:23:41 +0530

    almost done the revision

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,11 @@ from .views import *
 
 urlpatterns = [
     # Users URLs
-    path('users/', UsersListCreateView.as_view(), name='users-list-create'),
+    path('users', UsersListCreateView.as_view(), name='users-list-create'),
     path('users/<int:pk>/', UsersRetrieveUpdateDeleteView.as_view(), name='users-detail'),
 
     # Tasks URLs
-    path('tasks/', TasksListCreateView.as_view(), name='tasks-list-create'),
+    path('tasks', TasksListCreateView.as_view(), name='tasks-list-create'),
     path('tasks/<int:pk>/', TasksRetrieveUpdateDeleteView.as_view(), name='tasks-detail'),
 
     # CCode URLs
@@ -15,6 +15,6 @@ urlpatterns = [
     path('ccode/<int:pk>/', CCodeRetrieveUpdateDeleteView.as_view(), name='ccode-detail'),
 
     # AccessControl URLs
-    path('accesscontrol/', AccessControlListCreateView.as_view(), name='accesscontrol-list-create'),
+    path('accesscontrol', AccessControlListCreateView.as_view(), name='accesscontrol-list-create'),
     path('accesscontrol/<int:pk>/', AccessControlRetrieveUpdateDeleteView.as_view(), name='accesscontrol-detail'),
 ]
